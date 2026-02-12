ac34530d72520dff54e2f32e4ecf3fc0b738b3ac
fadimeeren <fadime_ern135@outlook.com>
2026-02-12 22:27:01 +0300

    ..

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,11 +1,27 @@
-import { devIcon } from "leaflet";
+import { divIcon } from "leaflet";
 
-export const getIcon = (direction) => {
-  return devIcon({
+export const getIcon = (direction, isActive, isDetailMode) => {
+  return divIcon({
     html: `
-    <div style= "transform: rotate (${direction - 45}deg)> <img src= "/plane.svg" width="30px" height="30px" /> 
-    </div>`,
+        <div style="transform: rotate(${direction - 45}deg)">
+            <img src="/plane.svg" width="30px" height="30px"/>
+        </div>
+        `,
     iconSize: [30, 30],
-    className: "marker",
+    className: `marker ${isDetailMode && "passive-flight"} ${isActive && "active-flight"}`,
+  });
+};
+
+export const formatDate = (unixTime) => {
+  // değer yoksa null dö
+  if (!unixTime || unixTime === 0) return null;
+
+  // saniye formatındaki veriyi milisaniye formatında çevir
+  const formatted = new Date(unixTime * 1000);
+
+  // kullanıcı dostur formata çevir
+  return formatted.toLocaleTimeString("tr", {
+    hour: "2-digit",
+    minute: "2-digit",
   });
 };
